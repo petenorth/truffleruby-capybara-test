a8c25284838e1ba922ea16121d33880106f55a3b
Peter Fry <3152937+petenorth@users.noreply.github.com>
2020-03-11 17:09:19 +0000

    Update test.rb

--- test.rb
+++ test.rb
@@ -7,10 +7,10 @@ include Capybara
 node = Capybara.string <<-HTML
      <div id="uk-telephone-number" class="govuk-summary-list__row uk-telephone-number">
         <dd class="govuk-summary-list__value">
-           Itâ€™s just an idea
+           It’s just an idea
         </dd>
       </div>
 HTML
 
-row_value = nil
+row_value = 'It’s just an idea'
 node.have_css('dd', text: /#{row_value}/i)
