5d4b024d04fbc8a2a04126505fa5810f4c70f874
Peter Fry <3152937+petenorth@users.noreply.github.com>
2020-03-11 11:43:10 +0000

    Update test.rb

--- test.rb
+++ test.rb
@@ -2,5 +2,19 @@ require 'capybara/rspec/matchers';
 
 include Capybara::RSpecMatchers
 
+node = Capybara.string <<-HTML
+     <div id="uk-telephone-number" class="govuk-summary-list__row uk-telephone-number">
+        <dt class="govuk-summary-list__key">
+          UK telephone number
+        </dt>
+        <dd class="govuk-summary-list__value">
+          07123 456789
+        </dd>
+          <dd class="govuk-summary-list__actions">
+            
+          </dd>
+      </div>
+HTML
+
 row_value = nil
-have_css('dd', text: /#{row_value}/i)
+node.have_css('dd', text: /#{row_value}/i)
